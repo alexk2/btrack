5c1f9082384c0590616aba0527ebb06fec36dd06
Alex Knight <alexk4096@gmail.com>
2016-06-29 17:09:17 +1000

    Add init_db() function

--- btrack/core.py
+++ btrack/core.py
@@ -1,2 +1,22 @@
+import sqlite3
+
+def init_db(db_path):
+
+	conn = sqlite3.connect(db_path)
+	c = conn.cursor()
+
+	c.execute('''
+	CREATE TABLE btrack 
+	(path TEXT PRIMARY KEY, 
+	hash TEXT, 
+	file_mod_time TEXT, 
+	hash_mod_time TEXT)
+	''')
+
+	c.execute('''
+	CREATE INDEX btrack_hash_idx
+	ON btrack(hash)
+	''')
+
 def main():
 	pass
