697a07660541c4cbee29557d3995d9a6e10974a3
Alex Knight <alexk4096@gmail.com>
2016-06-29 19:35:56 +1000

    Add 'with' statement to DB usage

--- btrack/core.py
+++ btrack/core.py
@@ -2,21 +2,23 @@ import sqlite3
 
 def init_db(db_path):
 
-	conn = sqlite3.connect(db_path)
-	c = conn.cursor()
+	with sqlite3.connect(db_path) as conn:
 
-	c.execute('''
-	CREATE TABLE btrack 
-	(path TEXT PRIMARY KEY, 
-	hash TEXT, 
-	file_mod_time TEXT, 
-	hash_mod_time TEXT)
-	''')
+		c = conn.cursor()
 
-	c.execute('''
-	CREATE INDEX btrack_hash_idx
-	ON btrack(hash)
-	''')
+		c.execute('''
+		CREATE TABLE btrack
+		(path TEXT PRIMARY KEY,
+		hash TEXT,
+		file_mod_time TEXT,
+		hash_mod_time TEXT)
+		''')
+
+		c.execute('''
+		CREATE INDEX btrack_hash_idx
+		ON btrack(hash)
+		''')
 
 def main():
+
 	pass
